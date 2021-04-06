68a7533efe0a24aab1d86166f35ab421abfb4a7c
Mixone-FinallyHere <miguel.terol.93@gmail.com>
2021-04-06 15:45:58 +0200

    Actually working. Adding configuration options for release

--- VPlusChatFilter/VPlusChatFilter.cs
+++ VPlusChatFilter/VPlusChatFilter.cs
@@ -11,85 +11,135 @@ namespace VPlusChatFilter
     [BepInPlugin(ID, "Valheim Plus Chat Filter", version)]
     public class VPlusChatFilter : BaseUnityPlugin
     {
-
         public const string ID = "com.mixone.valheimplus.chatfilter";
         public const string version = "0.0.1.0";
 
         public Harmony harmony;
 
         public static List<string> bannedWords = new List<string>() {"test", "dev", "insult"};
+        public static BepInEx.Logging.ManualLogSource chatFilterLogger;
 
         void Awake()
         {
             harmony = new Harmony(ID);
             harmony.PatchAll();
+            chatFilterLogger = Logger;
+
+            chatFilterLogger.LogInfo("This server is using VPlusCHatFilter.");
         }
     }
 
-    #region Patches
+    #region Patches 
 
     #region ZRoutedRPC
 
     [HarmonyPatch(typeof(ZRoutedRpc), nameof(ZRoutedRpc.RouteRPC))]
     public static class ZRoutedRPC_RouteRPC_Patch
     {
-        public static void Prefix(ref ZRoutedRpc __instance, ref ZRoutedRpc.RoutedRPCData rpcData)
+        public static bool Prefix(ref ZRoutedRpc __instance, ref ZRoutedRpc.RoutedRPCData rpcData)
         {            
             if (rpcData.m_methodHash == "ChatMessage".GetStableHashCode())
             {
-                ZPackage payload = rpcData.m_parameters;
-                ZPackage newpayload = new ZPackage();
-                Debug.Log("ChatMessage Sent");
+                ZPackage payload = rpcData.m_parameters;    
+                VPlusChatFilter.chatFilterLogger.LogDebug("ChatMessage Sent");
                 payload.SetPos(0);
-                Debug.Log("Size of package is " + payload.Size());
-                Debug.Log("Read byte test : " + payload.ReadInt());
+                VPlusChatFilter.chatFilterLogger.LogDebug("Size of package is " + payload.Size());
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read byte test : " + payload.ReadInt());
                 payload.SetPos(0);
                 Vector3 headPoint = payload.ReadVector3();
-                Debug.Log("Read head : " + headPoint.ToString());              
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read head : " + headPoint.ToString());              
                 int messageType = payload.ReadInt();
-                Debug.Log("Read type : " + messageType);
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read type : " + messageType);
                 string playerName = payload.ReadString();
-                Debug.Log("Read name : " + playerName);
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read name : " + playerName);
                 string message = payload.ReadString();
-                Debug.Log("Read message : " + message);
-                Debug.Log(message);
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read message : " + message);
                 foreach (string bannable in VPlusChatFilter.bannedWords)
                 {
                     if(message.ToLower().Contains(bannable))
                     {
-                        Debug.Log("Bad word from "+playerName);
-                        message.ToLower().Replace(bannable, "DENIED");                        
+                        VPlusChatFilter.chatFilterLogger.LogInfo("Bad word from "+playerName);
+                        message = message.ToLower().Replace(bannable, "DENIED");                        
                     }
                 }
+                VPlusChatFilter.chatFilterLogger.LogDebug("New message : " + message);
+                ZPackage newpayload = new ZPackage();
                 ZRpc.Serialize(new object[] { headPoint, messageType, playerName, message}, ref newpayload);
                 rpcData.m_parameters = newpayload;
             } else if (rpcData.m_methodHash == "Say".GetStableHashCode())
             {
                 ZPackage payload = rpcData.m_parameters;
-                ZPackage newpayload = new ZPackage();
-                Debug.Log("Say Sent");
+                VPlusChatFilter.chatFilterLogger.LogDebug("Say Sent");
                 payload.SetPos(0);
-                Debug.Log("Size of package is "+payload.Size());
-                Debug.Log("Read byte test : " + payload.ReadInt());
+                VPlusChatFilter.chatFilterLogger.LogDebug("Size of package is "+payload.Size());
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read byte test : " + payload.ReadInt());
                 payload.SetPos(0);
                 int messageType = payload.ReadInt();
-                Debug.Log("Read type : " + messageType);                
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read type : " + messageType);                
                 string playerName = payload.ReadString();
-                Debug.Log("Read name : "+playerName);
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read name : "+playerName);
                 string message = payload.ReadString();
-                Debug.Log("Read message : " + message);
-                Debug.Log(message);
+                VPlusChatFilter.chatFilterLogger.LogDebug("Read message : " + message);
                 foreach (string bannable in VPlusChatFilter.bannedWords)
                 {
                     if (message.ToLower().Contains(bannable))
                     {
-                        Debug.Log("Bad word from " + playerName);
-                        message.ToLower().Replace(bannable, "DENIED");
+                        VPlusChatFilter.chatFilterLogger.LogInfo("Bad word from " + playerName);
+                        message = message.ToLower().Replace(bannable, "DENIED");
                     }
                 }
+                VPlusChatFilter.chatFilterLogger.LogDebug("New message : " + message);
+                ZPackage newpayload = new ZPackage();
                 ZRpc.Serialize(new object[] { messageType, playerName, message }, ref newpayload);
                 rpcData.m_parameters = newpayload;
+            } 
+            ZPackage zpackage = new ZPackage();
+            rpcData.Serialize(zpackage);
+            if (__instance.m_server)
+            {
+                if (rpcData.m_targetPeerID != 0L)
+                {
+                    ZNetPeer peer = __instance.GetPeer(rpcData.m_targetPeerID);
+                    if (peer != null && peer.IsReady())
+                    {
+                        peer.m_rpc.Invoke("RoutedRPC", new object[]
+                        {
+                        zpackage
+                        });
+                        return false;
+                    }
+                    return false;
+                }
+                else
+                {
+                    using (List<ZNetPeer>.Enumerator enumerator = __instance.m_peers.GetEnumerator())
+                    {
+                        while (enumerator.MoveNext())
+                        {
+                            ZNetPeer znetPeer = enumerator.Current;
+                            if (rpcData.m_senderPeerID != znetPeer.m_uid && znetPeer.IsReady())
+                            {
+                                znetPeer.m_rpc.Invoke("RoutedRPC", new object[]
+                                {
+                                zpackage
+                                });
+                            }
+                        }
+                        return false;
+                    }
+                }
             }
+            foreach (ZNetPeer znetPeer2 in __instance.m_peers)
+            {
+                if (znetPeer2.IsReady())
+                {
+                    znetPeer2.m_rpc.Invoke("RoutedRPC", new object[]
+                    {
+                    zpackage
+                    });
+                }
+            }
+            return false;
         }
     }
 
